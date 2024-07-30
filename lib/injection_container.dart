c98881cb9c8c62be29947f720ab32b9c34570a06
Kaushal kishore sharma <kaushalkishoresharma07@gmail.com>
2024-07-30 06:33:57 +0530

    injection container & BlocProvider
    create injection container file for dependency injection and write code of bloc provider in main .

---
+++ lib/injection_container.dart
@@ -0,0 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:spotify/features/data/data_sources/firebase_remote_datasource.dart';
+import 'package:spotify/features/data/data_sources/firebase_remote_datasource_impl.dart';
+import 'package:spotify/features/data/repository/firebase_repo_impl.dart';
+import 'package:spotify/features/domain/repository/auth/auth_repo.dart';
+import 'package:spotify/features/domain/usecases/forgot_password_usecase.dart';
+import 'package:spotify/features/domain/usecases/get_create_current_user_usecase.dart';
+import 'package:spotify/features/domain/usecases/get_current_uid_usecase.dart';
+import 'package:spotify/features/domain/usecases/googleauthusercase.dart';
+import 'package:spotify/features/domain/usecases/sign_in_usecase.dart';
+import 'package:spotify/features/domain/usecases/sign_out_usecase.dart';
+import 'package:spotify/features/presentation/credential/credential_cubit.dart';
+import 'package:spotify/features/presentation/cubit/auth/auth_cubit.dart';
+
+final sl = GetIt.instance;
+
+Future<void>init() async{
+
+//cubit
+
+  sl.registerFactory<AuthCubit>(()=>AuthCubit(getCurrentUIDUseCase: sl.call(),signOutUseCase:sl.call() ,isSignInUseCase:sl.call() ));
+
+
+
+sl.registerLazySingleton<AuthCubit>(()=>AuthCubit(getCurrentUIDUseCase: sl.call(), isSignInUseCase:sl.call(), signOutUseCase:sl.call()));
+
+
+
+sl.registerLazySingleton<CredentialCubit>(()=>CredentialCubit(googleauthusercase: sl.call(), forgotPasswordUseCase:sl.call(), signInUseCase:sl.call(), signUpUseCase:sl.call(), getCreateCurrentUserUseCase:sl.call(),));
+
+
+//usecases
+sl.registerLazySingleton<GetCurrentUIDUseCase>(()=>GetCurrentUIDUseCase(repository: sl.call()));
+
+sl.registerLazySingleton(()=>SignOutUseCase(repository: sl.call()));
+
+sl.registerLazySingleton(()=>SignInUseCase(repository:sl.call() ));
+
+sl.registerLazySingleton(()=>ForgotPasswordUseCase(repository:sl.call()));
+
+sl.registerLazySingleton(()=>GetCreateCurrentUserUseCase(repository:sl.call()));
+
+sl.registerLazySingleton(()=>Googleauthusercase(repository:sl.call() ));
+
+
+
+sl.registerLazySingleton<AuthRepository>(()=>FirebaseRepoImpl(remoteDatasource:sl.call()));
+
+sl.registerLazySingleton<FirebaseRemoteDatasource>(()=>FirebaseRemoteDatasourceImpl(auth:sl.call() ,firestore:sl.call() ,googleSignIn:sl.call() ));
+
+
+//external
+final auth=FirebaseAuth.instance;
+final firestore=FirebaseFirestore.instance;
+final googlesignin=GoogleSignIn();
+
+sl.registerLazySingleton(()=>auth);
+sl.registerLazySingleton(()=>firestore);
+sl.registerLazySingleton(()=>googlesignin);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -5,34 +5,43 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:spotify/features/presentation/bloc/themebackend.dart';
+import 'package:spotify/features/presentation/credential/credential_cubit.dart';
+import 'package:spotify/features/presentation/cubit/auth/auth_cubit.dart';
 import 'package:spotify/firebase_options.dart';
 import 'core/config/theme/theme.dart';
 import 'features/presentation/Pages/Splash/pages/splash.dart';
+import 'package:spotify/injection_container.dart ' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   HydratedBloc.storage = await HydratedStorage.build(
-      storageDirectory: kIsWeb
-          ? HydratedStorage.webStorageDirectory
-          : await getApplicationDocumentsDirectory(),
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getApplicationDocumentsDirectory(),
   );
 
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-
-    return MultiBlocProvider(providers:[BlocProvider(create:(_)=>Themebackend())],
-child:BlocBuilder<Themebackend,ThemeMode>(builder: (context,mode)=>MaterialApp(theme:Themesname.lightTheme,darkTheme: Themesname.darkTheme,themeMode: mode,debugShowCheckedModeBanner: false,home: Splash(),),),);
-
+    return MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => Themebackend()),
+      BlocProvider(create:(_)=>di.sl<AuthCubit>()..appstarted()),BlocProvider(create: (_)=>di.sl<CredentialCubit>())],
+      child: BlocBuilder<Themebackend, ThemeMode>(
+        builder: (context, mode) => MaterialApp(
+          theme: Themesname.lightTheme,
+          darkTheme: Themesname.darkTheme,
+          themeMode: mode,
+          debugShowCheckedModeBanner: false,
+          home: Splash(),
+        ),
+      ),
+    );
   }
 }
-
